9cab693503db8426e98cab89f3ec9ed5a9cdf662
Рустам Каюмов <krr@pkzdrav.ru>
2020-09-15 00:00:16 +0500

    Валидация пути и параметров get-запросов

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 from schemas import Book
+from typing import List
+
+
 app = FastAPI()
 
 @app.get('/')
@@ -11,6 +14,7 @@ def get_item(pk: int, q: str = None):
     # указывание типов данных, происходит автоматическая валидция входящих данных
     return {"key": pk,
             "q": q}
+
 @app.get('/user/{pk}/items/{item}')
 def get_user_item(pk: int, item: str):
     return {"user": pk, "item": item}
@@ -18,3 +22,23 @@ def get_user_item(pk: int, item: str):
 @app.post('/book/')
 def create_book(item: Book):
     return item
+
+@app.get('/book')
+def get_book(q: str = Query(..., min_length=2, max_length=5, description='Search book', regex=r'[a-z]{2,5}')):
+    # Query позволяет валидировать параметры get-запроса
+    # None указывает на необязательность запроса
+    # ... Ellipsis указывает на обязательность параметра
+    # или же явно указать default value
+    # q: List(str) = Query( - можем указать список параметров /?q=some&q=another&q=some_another
+    # deprecated = True - флаг указывающий, что данный парметр устарел
+    return q
+
+
+@app.get('/book/{pk}')
+def get_single_book(pk: int = Path(..., gt=1, le=10), pages: int = Query(None, gt=10, le=500)):
+    # Path - валидация пути параметра pk
+    # ... - указывает на обязательность pk
+    # gt=1 - great than
+    # le=10 - pk не должно быть более 10
+    return {"pk": pk,
+            "pges": pages}
