98c07db3b0f5f4eaa0bb6e53da086e1037fadb63
Рустам Каюмов <krr@pkzdrav.ru>
2020-09-15 00:22:08 +0500

    Пример простейшей валидации в pydantic

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, Field
 from datetime import date
 from typing import List
 #Валидация данных. Возвращает объект
@@ -7,9 +7,21 @@ class Genre(BaseModel):
     name: str
 
 class Author(BaseModel):
-    first_name: str
+    first_name: str = Field(..., max_length=25)
     last_name: str
-    age: int
+    age: int = Field(
+        ...,
+        gt=15,
+        lt=90,
+        description='Автору не может быть менее 15 и не более 90 лет'
+    )
+
+    @validator('last_name')
+    def check_age(cls, v):
+        #кастомная валидация поля
+        if len(v) < 3:
+            raise ValueError('Не корректная фамилия')
+        return
 
 class Book(BaseModel):
     title: str
